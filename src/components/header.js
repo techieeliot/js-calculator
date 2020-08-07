bce9792a47138f687892cb3be77c51adbeaf6ea4
Eliot Sanford <eliot587@gmail.com>
2020-08-07 18:09:37 -0500

    Add new site title for js calculator from gatsby starter

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#0B6887`,
       marginBottom: `1.45rem`,
     }}
   >
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,15 +7,20 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    <SEO 
+      title="Calculator"
+      description="A Calculator created by Eliot Sanford created as one of five projects for the freeCodeCamp Front-End Library Certification."
+      lang="en"
+      meta=""
+      // keywords="Calculator, calc, caculators, calculator, calcalator, Eliot, Eliot Sanford, Eliot Stanford, Elliott, Elliot, Stanford, Sanford, Sandford, Eliot Sandford, projects, freeCodeCamp, freccodecamp, Code Camp, Free Code Camp, Reactjs, React, Front-End, Frontend, Library, Certification"
+    />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>Welcome to my Calculator</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+      {/* <Image /> */}
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    {/* <Link to="/page-2/">Go to page 2</Link> <br /> */}
+    {/* <Link to="/using-typescript/">Go to "Using TypeScript"</Link> */}
   </Layout>
 )
 
